49f08ae1d49620df563f952aab2be09604220ea3
[Nivedita Ranjan] <[niveditaranjan1998@gmail.com]>
2023-07-07 23:54:07 +0530

    Local data is sync with the Realtime Database

--- firebaseapp/src/App.js
+++ firebaseapp/src/App.js
@@ -1,6 +1,6 @@
 import './App.css';
 import { initializeApp } from "https://www.gstatic.com/firebasejs/9.15.0/firebase-app.js"
-import { getDatabase , ref , push } from "https://www.gstatic.com/firebasejs/9.15.0/firebase-database.js"
+import { getDatabase , ref , push , onValue} from "https://www.gstatic.com/firebasejs/9.15.0/firebase-database.js"
 
 const appSettings = {
     databaseURL: "https://playground-2dcfd-default-rtdb.asia-southeast1.firebasedatabase.app"
@@ -15,12 +15,46 @@ const addBtn = document.getElementById("add-btn")
 const itemList = document.getElementById("item-list")
 
 addBtn.addEventListener("click", function() {
-   let inputValue = inputFieldEl.Value
+   let inputValue = inputFieldEl.value
+
+   clearInputFieldEl()
    
    push(shoppingListInDB, inputValue)
    
-   itemList.innerhtml += `<li>${inputValue}</li>`
 })
+
+onValue(shoppingListInDB , function(snapshot){
+    let itemsArray = Object.values(snapshot.val()) //converting data object in array
+
+    clearShoppingList()
+
+    for (let i = 0; i < itemsArray.length; i++){
+        addItemToShoppingList(itemsArray[i])
+    }
+})
+
+function clearShoppingList(){
+    itemList.innerHTML = ""
+}
+
+function clearInputFieldEl(){
+    inputFieldEl.value = ""
+}
+
+function addItemToShoppingList(itemValue){
+
+    itemList.innerHTML += `<li>${itemValue}</li>`
+}
+
+
+
+
+
+
+
+
+
+
 
 
 function App() {}
