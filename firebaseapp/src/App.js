2c736b0c6082c727c9b377479724a5929e271817
[Nivedita Ranjan] <[niveditaranjan1998@gmail.com]>
2023-07-08 13:02:45 +0530

    Delete functionality added

--- firebaseapp/src/App.js
+++ firebaseapp/src/App.js
@@ -1,6 +1,6 @@
 import './App.css';
 import { initializeApp } from "https://www.gstatic.com/firebasejs/9.15.0/firebase-app.js"
-import { getDatabase , ref , push , onValue} from "https://www.gstatic.com/firebasejs/9.15.0/firebase-database.js"
+import { getDatabase , ref , push , onValue , remove } from "https://www.gstatic.com/firebasejs/9.15.0/firebase-database.js"
 
 const appSettings = {
     databaseURL: "https://playground-2dcfd-default-rtdb.asia-southeast1.firebasedatabase.app"
@@ -17,19 +17,30 @@ const itemList = document.getElementById("item-list")
 addBtn.addEventListener("click", function() {
    let inputValue = inputFieldEl.value
 
-   clearInputFieldEl()
-   
    push(shoppingListInDB, inputValue)
+   
+   clearInputFieldEl()
    
 })
 
 onValue(shoppingListInDB , function(snapshot){
-    let itemsArray = Object.values(snapshot.val()) //converting data object in array
 
-    clearShoppingList()
-
-    for (let i = 0; i < itemsArray.length; i++){
-        addItemToShoppingList(itemsArray[i])
+    if(snapshot.exists()){
+        
+        let itemsArray = Object.entries(snapshot.val()) 
+        
+        //object.values --> it will get the values and convert it into array
+        //object.entries is used to get both keys and values from database
+    
+        clearShoppingList()
+    
+        for (let i = 0; i < itemsArray.length; i++){
+            let currentItem = itemsArray[i]
+            
+            addItemToShoppingList(currentItem)
+        }
+    } else {
+        itemList.innerHTML = "No item in the shopping List..."
     }
 })
 
@@ -41,9 +52,23 @@ function clearInputFieldEl(){
     inputFieldEl.value = ""
 }
 
-function addItemToShoppingList(itemValue){
+function addItemToShoppingList(item){
+    let itemID = item[0]
+    let itemValue = item[1]
 
-    itemList.innerHTML += `<li>${itemValue}</li>`
+    let newEl = document.createElement("li") 
+    
+    // newEl -->  (bunch of <li>'s)
+
+    newEl.textContent = itemValue
+
+   newEl.addEventListener("click" , function(){
+        let exactLocationOfItemInDB = ref(database , `ShoppingList/${itemID}`)
+
+        remove(exactLocationOfItemInDB)
+    })
+
+    itemList.append(newEl)
 }
 
 
